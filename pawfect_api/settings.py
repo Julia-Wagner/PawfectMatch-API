f3ef0e03f1f3e344c32cf98218bdcefc43223a0f
julia Wager <julia@p-wagner.at>
2024-03-30 09:26:18 +0100

    feat: add posts app with model

--- pawfect_api/settings.py
+++ pawfect_api/settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'profiles',
+    'posts',
 ]
 SITE_ID = 1
 MIDDLEWARE = [
---
+++ posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    """
+    Post model.
+    Images handled in separate medias app.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='posts')
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
