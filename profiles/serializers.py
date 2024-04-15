1b46af57bf68de08a73f23503f696d64ecc99642
julia Wager <julia@p-wagner.at>
2024-04-15 12:42:03 +0200

    feat: add following field to profiles serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from followers.models import Follower
 from .models import Profile
 from django_countries.serializers import CountryFieldMixin
 
@@ -6,15 +8,25 @@ from django_countries.serializers import CountryFieldMixin
 class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'address_1', 'address_2', 'city', 'postcode',
-            'country', 'description', 'type', 'image', 'is_owner'
+            'address_1', 'address_2', 'city', 'postcode', 'country',
+            'description', 'type', 'image', 'is_owner', 'following_id'
         ]
