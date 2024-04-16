e3a8cb6c3dcd9f374c8c0c83b98ccde4c0c46152
julia Wager <julia@p-wagner.at>
2024-04-16 07:40:38 +0200

    feat: add save field to post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from dogs.models import Dog
 from .models import Post
+from saves.models import Save
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -11,14 +12,25 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     dogs = serializers.PrimaryKeyRelatedField(
         queryset=Dog.objects.all(), many=True, required=False)
+    save_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_save_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            save = Save.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return save.id if save else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'title', 'type', 'content', 'created_at', 'updated_at', 'dogs'
+            'title', 'type', 'content', 'created_at', 'updated_at', 'dogs',
+            'save_id'
         ]
