d03877c335a5c8120fe85a5eb3104993e8647a9c
julia Wager <julia@p-wagner.at>
2024-04-16 07:29:39 +0200

    feat: add saves serializer

---
+++ saves/serializers.py
@@ -0,0 +1,20 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from saves.models import Save
+
+
+class SaveSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Save model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Save
+        fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
