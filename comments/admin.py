b36e2f1de7a84d5b83042f8a819d9d1c849119cb
julia Wager <julia@p-wagner.at>
2024-05-02 12:31:33 +0200

    feat: register all models to admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BannedWord
+from .models import BannedWord, Comment
 
+admin.site.register(Comment)
 # register banned word model to allow superusers to add/edit the words
 admin.site.register(BannedWord)
---
+++ dogs/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import DogCharacteristic, Dog
+
+admin.site.register(DogCharacteristic)
+admin.site.register(Dog)
