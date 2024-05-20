7260e4b429e94b6d36831be84a30903a84058348
julia Wager <julia@p-wagner.at>
2024-05-20 20:55:57 +0200

    fix: validate image and video separately

--- medias/serializers.py
+++ medias/serializers.py
@@ -9,38 +9,46 @@ class MediaSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
+    def validate_image(self, image):
+        """
+        Validate the uploaded image
+        """
+        if image.size > 5 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 5MB!')
+        if image.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!')
+        if image.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!')
+        return image
+
+    def validate_video(self, video):
+        """
+        Validate the uploaded video
+        """
+        if video.size > 50 * 1024 * 1024:
+            raise serializers.ValidationError('Video size larger than 50MB!')
+        if not video.name.endswith('.mp4'):
+            raise serializers.ValidationError('Only MP4 video format allowed!')
+        return video
+
     def validate(self, attrs):
         """
-        Validate the uploaded media
+        Additional validation for the media type
         """
-        # get the type to validate
         media_type = attrs.get('type')
 
         if media_type == 'image':
             image = attrs.get('image')
-            if image:
-                if image.size > 2 * 1024 * 1024:
-                    raise serializers.ValidationError(
-                        'Image size larger than 2MB!')
-                if image.image.height > 4096:
-                    raise serializers.ValidationError(
-                        'Image height larger than 4096px!'
-                    )
-                if image.image.width > 4096:
-                    raise serializers.ValidationError(
-                        'Image width larger than 4096px!'
-                    )
+            if not image:
+                raise serializers.ValidationError('No image uploaded!')
+            self.validate_image(image)
         elif media_type == 'video':
             video = attrs.get('video')
-            if video:
-                if video.size > 50 * 1024 * 1024:
-                    raise serializers.ValidationError(
-                        'Video size larger than 50MB!')
-                if not video.name.endswith('.mp4'):
-                    raise serializers.ValidationError(
-                        'Only MP4 video format allowed!')
-            else:
+            if not video:
                 raise serializers.ValidationError('No video uploaded!')
+            self.validate_video(video)
         else:
             raise serializers.ValidationError('Invalid media type!')
 
